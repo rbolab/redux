cc48025d35c3b5b6f6ef5c25eed580ecd1274b9b
biba <biba@gmail.com>
2017-08-05 17:17:33 +0200

    step 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MdButtonModule, MdCardModule, MdMenuModule, MdToolbarModule, MdIconModule } from '@angular/material';
 import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { InMemoryStoryService } from '../api/in-memory-story.service';
 import { AppRoutingModule } from './app-routing.module';
@@ -10,6 +8,11 @@ import { CourseComponent } from "./funds/course.component";
 import { CourseListComponent } from "./funds/course-list.component";
 import { CourseService } from "./funds/course.service";
 import { AppComponent } from './app.component';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MdButtonModule, MdCardModule, MdMenuModule, MdToolbarModule, MdIconModule } from '@angular/material';
+import "hammerjs";
+
 
 @NgModule({
   declarations: [
